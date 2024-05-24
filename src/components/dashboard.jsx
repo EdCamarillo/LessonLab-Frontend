99372d8975257abe1258cafc2ef8364e9e8e9777
HoneyHanny <hansduran25@gmail.com>
2024-05-24 20:56:25 +0800

    Change title

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -6,7 +6,6 @@ import Quiz from '../assets/quiz.png';
 import '../styles/addNewDocumentPopUpWindow.css';
 import addNewDocument from '../assets/addNewDocument.png';
 
-
 const Dashboard = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
@@ -25,40 +24,55 @@ const Dashboard = () => {
       {showPopup && <div className="overlay" onClick={togglePopup}></div>}
       <div id="dashboard-container" className={showPopup ? 'popup-open' : ''}>
         <div className={`dashboard ${showPopup ? 'opacity-low' : ''}`}>
-          <div className='newDocument' onClick={togglePopup}>
-            <img src={addNewDocument} alt="addNewDocument" className="addNewDocument" />
+          <div className="newDocument" onClick={togglePopup}>
+            <img
+              src={addNewDocument}
+              alt="addNewDocument"
+              className="addNewDocument"
+            />
             <p>New Document</p>
           </div>
         </div>
         {showPopup && (
           <div className="popup-container">
             <div className="popup-content">
-              <span className="popup-close" onClick={togglePopup}>X</span>
+              <span className="popup-close" onClick={togglePopup}>
+                X
+              </span>
               <img src={icon} alt="icon" />
               <h2>You'd like to create a...</h2>
-             
-              <div className={`newLesson ${selectedOption === 'lesson' ? 'selected' : ''}`} onClick={() => handleOptionSelect('lesson')}>
+
+              <div
+                className={`newLesson ${
+                  selectedOption === 'lesson' ? 'selected' : ''
+                }`}
+                onClick={() => handleOptionSelect('lesson')}
+              >
                 <img src={Lesson} alt="addLesson" className="addLesson" />
                 <p>Lesson</p>
               </div>
 
-              <div className={`newQuiz ${selectedOption === 'quiz' ? 'selected' : ''}`} onClick={() => handleOptionSelect('quiz')}>
+              <div
+                className={`newQuiz ${
+                  selectedOption === 'quiz' ? 'selected' : ''
+                }`}
+                onClick={() => handleOptionSelect('quiz')}
+              >
                 <img src={Quiz} alt="addQuiz" className="addQuiz" />
                 <p>Quiz</p>
               </div>
 
-              <div className='enterTitle'>
-                <input placeholder='   Enter a title...'></input>
+              <div className="enterTitle">
+                <input placeholder="   Enter a title..."></input>
               </div>
 
               <button>Confirm</button>
-
             </div>
           </div>
         )}
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,19 +1,19 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/sideBar.css';
 import addButton from '../assets/addButton.png';
 import '../styles/addNewDocumentPopUpWindow.css';
 
 const SideBar = () => {
   return (
-    <div id="sideBar-container">
+    <div id="sideBar-container" style={{ userSelect: 'none' }}>
       <div className="sideBar">
-         <div className='docu'>
-            <h1>My documents</h1>
-            <img src={addButton} alt="addButton" className="addButton" />
-         </div>
+        <div className="docu">
+          <h1>My documents</h1>
+          <img src={addButton} alt="addButton" className="addButton" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
