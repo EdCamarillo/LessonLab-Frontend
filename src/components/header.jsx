93de641a32e50e2c18d091e9d489b95e77edd6bc
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2024-05-25 16:24:58 +0800

    Initial integration for paymongo api checkout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,31 @@ import profileIcon from '../assets/profileIcon.png';
 import { Link, useNavigate } from 'react-router-dom'
 import '../styles/header.css';
 import '../styles/addNewDocumentPopUpWindow.css';
+import createCheckoutSession from '../server/paymongoApi.js';
 
 
+//TODO: IMPLEMENT NAVIGATION TO PAYMONGO CHECKOUT SESSION (preferably in token shop or billing info screen)
+//TODO: POSSIBLE BILLING INFO SAVING
 const Header = () => {
+  // const navigate = useNavigate();
+  const handleBuyTokens = async () => {
+    try {
+      //NOTE: PASS ITEM PARAMETER WHEN WE MAKE MORE TOKEN VARIANTS
+      const response = await createCheckoutSession();
+      
+      if (response && response.data) {
+        console.log(response.data.data.attributes.checkout_url);
+
+
+        // navigate(response.data.data.attributes.checkout_url);
+      } else {
+        console.error("Invalid response data:", response);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   return (
     <div id="header-container">
      
@@ -13,7 +35,9 @@ const Header = () => {
         <img src={icon} alt="icon" />
         <span className="name">LessonLab</span>
 
-        <img src={profileIcon} alt="Profile" className="profile-button" /> 
+        {/* TODO: POSSIBLE TOKEN SHOP SCREEN AND BILLING INFO SCREEN FOR ONCLICK*/}
+        <button className='buy-button' onClick={handleBuyTokens}>Buy Tokens</button>
+        <img src={profileIcon} alt="Profile" className="profile-button"/> 
       </div>
     </div>
   )
