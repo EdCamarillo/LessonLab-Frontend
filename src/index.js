d858f5e7d9bf7cb0ba86cb033a90a49cb1826ec0
HoneyHanny <hansduran25@gmail.com>
2024-05-28 10:14:27 +0800

    Add quiz.jsx

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Dashboard from './pages/dashboard.jsx'
 import Lesson from './pages/lesson.jsx'
+import Quiz from './pages/quiz.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />
+    element: <Dashboard />,
   },
   {
     path: '/lesson',
-    element: <Lesson/> 
-  }
+    element: <Lesson />,
+  },
+  {
+    path: '/quiz',
+    element: <Quiz />,
+  },
 ])
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
---
+++ src/pages/quiz.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Quiz = () => {
+  return (
+    <>
+    <Header />
+    Quiz generation page
+    </>
+  )
+}
+
+export default Quiz
